c2bc9bc1c75e292e17fb54506e51d7407028dd64
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2018-10-22 10:42:28 +0700

    organize build query

--- src/buildQuery.js
+++ src/buildQuery.js
@@ -11,125 +11,74 @@ const validOp = op => {
 }
 
 module.exports = function(options) {
-  if (get(options, 'cache', false)) {
-    return memoize(
-      function(query, queryOptions) {
-        const promise = new Promise((resolve, reject) => {
-          if (!queryOptions) {
-            return resolve(query)
-          }
-
-          if (queryOptions.query) {
-            query.where(queryOptions.query)
-          }
-
-          if (queryOptions.skip) {
-            query.skip(queryOptions.skip)
-          }
-
-          if (options.limit && (!queryOptions.limit || queryOptions.limit === '0' || queryOptions.limit > options.limit)) {
-            queryOptions.limit = options.limit
-          }
-
-          if (queryOptions.limit && query.op !== 'count' && !queryOptions.distinct) {
-            query.limit(queryOptions.limit)
-          }
-
-          if (queryOptions.sort) {
-            query.sort(queryOptions.sort)
-          }
-
-          if (queryOptions.populate) {
-            query.populate(queryOptions.populate)
-          }
-
-          if (queryOptions.select) {
-            query.select(queryOptions.select)
-          }
-
-          if (queryOptions.distinct) {
-            query.distinct(queryOptions.distinct)
-          }
-
-          if (options.readPreference) {
-            query.read(options.readPreference)
-          }
-
-          if (options.lean) {
-            query.lean(options.lean)
-          }
-
-          resolve(query)
-        })
-
-        return promise
-      },
-      {
-        promise: true,
-        maxAge: get(options, 'cache_age', 5000),
-        profileName: 'Query Funciton',
-        normalizer: function(args) {
-          const arg = {
-            collectionName: get(args[0], 'mongooseCollection.collectionName', ''),
-            op: validOp(get(args[0], 'op', '')),
-            query: get(args, '[1]', {}),
-            conditions: get(args[0], '_conditions', '')
-          }
-          return JSON.stringify(arg)
-        }
+  const doQueryJob = function(query, queryOptions) {
+    const promise = new Promise((resolve, reject) => {
+      if (!queryOptions) {
+        return resolve(query)
       }
-    )
+
+      if (queryOptions.query) {
+        query.where(queryOptions.query)
+      }
+
+      if (queryOptions.skip) {
+        query.skip(queryOptions.skip)
+      }
+
+      if (options.limit && (!queryOptions.limit || queryOptions.limit === '0' || queryOptions.limit > options.limit)) {
+        queryOptions.limit = options.limit
+      }
+
+      if (queryOptions.limit && query.op !== 'count' && !queryOptions.distinct) {
+        query.limit(queryOptions.limit)
+      }
+
+      if (queryOptions.sort) {
+        query.sort(queryOptions.sort)
+      }
+
+      if (queryOptions.populate) {
+        query.populate(queryOptions.populate)
+      }
+
+      if (queryOptions.select) {
+        query.select(queryOptions.select)
+      }
+
+      if (queryOptions.distinct) {
+        query.distinct(queryOptions.distinct)
+      }
+
+      if (options.readPreference) {
+        query.read(options.readPreference)
+      }
+
+      if (options.lean) {
+        query.lean(options.lean)
+      }
+
+      resolve(query)
+    })
+
+    return promise
+  }
+
+  if (get(options, 'cache', false)) {
+    return memoize(doQueryJob, {
+      promise: true,
+      maxAge: get(options, 'cache_age', 5000),
+      profileName: 'Query Funciton',
+      normalizer: function(args) {
+        const arg = {
+          collectionName: get(args[0], 'mongooseCollection.collectionName', ''),
+          op: validOp(get(args[0], 'op', '')),
+          query: get(args, '[1]', {}),
+          conditions: get(args[0], '_conditions', '')
+        }
+        return JSON.stringify(arg)
+      }
+    })
   } else {
-    return function(query, queryOptions) {
-      const promise = new Promise((resolve, reject) => {
-        if (!queryOptions) {
-          return resolve(query)
-        }
-
-        if (queryOptions.query) {
-          query.where(queryOptions.query)
-        }
-
-        if (queryOptions.skip) {
-          query.skip(queryOptions.skip)
-        }
-
-        if (options.limit && (!queryOptions.limit || queryOptions.limit === '0' || queryOptions.limit > options.limit)) {
-          queryOptions.limit = options.limit
-        }
-
-        if (queryOptions.limit && query.op !== 'count' && !queryOptions.distinct) {
-          query.limit(queryOptions.limit)
-        }
-
-        if (queryOptions.sort) {
-          query.sort(queryOptions.sort)
-        }
-
-        if (queryOptions.populate) {
-          query.populate(queryOptions.populate)
-        }
-
-        if (queryOptions.select) {
-          query.select(queryOptions.select)
-        }
-
-        if (queryOptions.distinct) {
-          query.distinct(queryOptions.distinct)
-        }
-
-        if (options.readPreference) {
-          query.read(options.readPreference)
-        }
-
-        if (options.lean) {
-          query.lean(options.lean)
-        }
-
-        resolve(query)
-      })
-
-      return promise
-    }
+    return doQueryJob
   }
 }
